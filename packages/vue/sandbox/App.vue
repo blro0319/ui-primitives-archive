12ade01691165482a8274bb86cf6cdf902288488
Blro <gyeongtae020319@gmail.com>
2023-05-01 06:38:44 +0900

    chore: Setup router of sandbox

--- packages/vue/sandbox/App.vue
+++ packages/vue/sandbox/App.vue
@@ -1,13 +1,3 @@
-<script setup lang="ts">
-import { VButton } from "@blro/ui-primitive-vue";
-import { ref } from "vue";
-
-const count = ref(0);
-</script>
-
 <template>
-  <div class="app">
-    <h1>Blro UI Primitive Vue</h1>
-    <VButton @click="count++">Counter: {{ count }}</VButton>
-  </div>
+  <RouterView />
 </template>
---
+++ packages/vue/sandbox/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { VButton } from "@/components";
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <div>
+    <h1>Blro UI Primitive Vue</h1>
+    <VButton @click="count++">Counter: {{ count }}</VButton>
+  </div>
+</template>
