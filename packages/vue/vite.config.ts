6c5ffef317734ea6b4e3a5f5d03315184e9e99c7
Blro <gyeongtae020319@gmail.com>
2023-05-03 21:05:29 +0900

    build: Setup for jsx

--- packages/vue/vite.config.ts
+++ packages/vue/vite.config.ts
@@ -1,9 +1,10 @@
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
   build: {
     outDir: "./dist",
     lib: {
