d49029feaaf9155cd7cb0e6ce1b8c1fe98bef983
Blro <gyeongtae020319@gmail.com>
2023-05-03 22:47:02 +0900

    feat: VDialog component

--- packages/vue/sandbox/pages/index.vue
+++ packages/vue/sandbox/pages/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { VButton } from "@/components";
+import { VButton, VDialog } from "@/components";
 import { ref } from "vue";
 
 const count = ref(0);
+const dialog = ref<InstanceType<typeof VDialog>>();
 </script>
 
 <template>
@@ -11,5 +12,28 @@ const count = ref(0);
     <VButton @click="count++">Counter: {{ count }}</VButton>
     <VButton to="/foo">Foo</VButton>
     <VButton href="https://vuejs.org">Vue.js</VButton>
+    <VButton @click="dialog?.show()">Show</VButton>
+    <VButton @click="dialog?.showModal()">Show Modal</VButton>
+    <VDialog :transition="{}" ref="dialog">
+      Dialog
+      <VButton @click="dialog?.close()">Close</VButton>
+    </VDialog>
+    <VDialog>
+      Dialog
+      <VButton @click="dialog?.close()">Close</VButton>
+    </VDialog>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.v {
+  &-enter-active,
+  &-leave-active {
+    transition: opacity 500ms;
+  }
+  &-enter-from,
+  &-leave-to {
+    opacity: 0;
+  }
+}
+</style>
