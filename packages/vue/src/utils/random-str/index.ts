a1be5311ff78259ac93681bcb9b5606c7b13028d
Blro <gyeongtae020319@gmail.com>
2023-05-04 23:03:28 +0900

    feat: Add randomInt and randomStr utils

---
+++ packages/vue/src/utils/random-str/index.ts
@@ -0,0 +1,12 @@
+import { ALPHABETS, NUMBERS } from "@/constants";
+import { randomInt } from "@/utils";
+
+const POOL = ALPHABETS + NUMBERS;
+
+export function randomStr(length: number = 6) {
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += POOL.charAt(randomInt(0, POOL.length - 1));
+  }
+  return result;
+}
