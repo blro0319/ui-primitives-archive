d406483843a4b5d96ba639a7597d71cf4b3f04d8
Blro <gyeongtae020319@gmail.com>
2023-05-08 15:02:00 +0900

    chore: Add vCompoundType in ComponentCustomOptions
    Using for child search on root of compound component

--- packages/vue/env.d.ts
+++ packages/vue/env.d.ts
@@ -1,6 +1,10 @@
 /// <reference types="vite/client" />
 
 declare module "vue" {
+  interface ComponentCustomOptions {
+    vCompoundType?: string;
+  }
+
   interface Events {
     // dialog events
     onCancel: Event;
