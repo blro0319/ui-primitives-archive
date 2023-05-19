96294b8df9d24f707b7a17ab3a02800bc94c22c4
Blro <gyeongtae020319@gmail.com>
2023-05-19 17:30:16 +0900

    fix: Move module declare to root from env.d.ts

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -2,3 +2,15 @@ export * from "./components";
 export * from "./composables";
 export * from "./types";
 export * from "./utils";
+
+export type VCompoundType = "VSelectOption" | "VTabsTrigger";
+declare module "vue" {
+  interface ComponentCustomOptions {
+    vCompoundType?: VCompoundType;
+  }
+  interface Events {
+    // dialog events
+    onCancel: Event;
+    onClose: Event;
+  }
+}
