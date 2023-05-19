14f62fc4f73e126951cc1b68ee9f743838e7ca87
Blro <gyeongtae020319@gmail.com>
2023-05-19 16:43:57 +0900

    chore: Add multiple example in VSelect playground

--- packages/vue/playground/pages/component/select.vue
+++ packages/vue/playground/pages/component/select.vue
@@ -8,22 +8,32 @@ import {
 import { ref } from "vue";
 
 const value = ref<string>("");
+const multiple = ref<string[]>([]);
+
 const OPTIONS = ["Option 1", "Option 2", "Option 3"];
 </script>
 
 <template>
   <div class="select">
     <h1>VSelect</h1>
-    <VSelect v-model="value">
-      <VSelectTrigger>{{ value }}</VSelectTrigger>
-      <VSelectMenu>
-        <VSelectOption
-          v-for="option in OPTIONS"
-          :value="option"
-          :label="option"
-        />
-      </VSelectMenu>
-    </VSelect>
+    <article>
+      <h2>Basic Usage</h2>
+      <VSelect v-model="value">
+        <VSelectTrigger>{{ value || "-" }}</VSelectTrigger>
+        <VSelectMenu>
+          <VSelectOption v-for="option in OPTIONS" :value="option" />
+        </VSelectMenu>
+      </VSelect>
+    </article>
+    <article>
+      <h2>Multiple</h2>
+      <VSelect v-model="multiple" multiple>
+        <VSelectTrigger>{{ multiple.join(", ") || "-" }}</VSelectTrigger>
+        <VSelectMenu>
+          <VSelectOption v-for="option in OPTIONS" :value="option" />
+        </VSelectMenu>
+      </VSelect>
+    </article>
   </div>
 </template>
 
