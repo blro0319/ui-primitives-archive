0f33868e212a94e42ce50d528d763e077e7a0996
Blro <gyeongtae020319@gmail.com>
2023-05-19 17:39:25 +0900

    feat: filterCompoundVNodes util

--- packages/vue/src/utils/match-compound-type/index.ts
+++ packages/vue/src/utils/match-compound-type/index.ts
@@ -1,6 +1,7 @@
 import type { VNode } from "vue";
+import type { VCompoundType } from "~/index";
 
-export function matchCompoundType(target: VNode, match: string) {
+export function matchCompoundType(target: VNode, match: VCompoundType) {
   const type = target.type;
   if (typeof type !== "object" || !("vCompoundType" in type)) return false;
   return type.vCompoundType === match;
