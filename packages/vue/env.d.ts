724b84ff1e514272a8f81184ee8108ffb54c388b
Blro <gyeongtae020319@gmail.com>
2023-05-19 17:30:16 +0900

    fix: Move module declare to root from env.d.ts

--- packages/vue/env.d.ts
+++ packages/vue/env.d.ts
@@ -1,15 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module "vue" {
-  interface ComponentCustomOptions {
-    vCompoundType?: string;
-  }
-
-  interface Events {
-    // dialog events
-    onCancel: Event;
-    onClose: Event;
-  }
-}
-
-export {};
