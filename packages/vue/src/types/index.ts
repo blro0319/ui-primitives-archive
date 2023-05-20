8a657b32b5c5ee069be871ba586fcc17225008bd
Blro <gyeongtae020319@gmail.com>
2023-05-20 17:03:52 +0900

    refactor: Move types file

--- packages/vue/src/types/index.ts
+++ packages/vue/src/types/index.ts
@@ -1,1 +1,6 @@
-export * from "./utils";
+import type { Component } from "vue";
+
+export type ComponentAs = string | Component;
+
+export type MaybeArray<T> = T | T[];
+export type MaybePromise<T> = T | PromiseLike<T>;
--- packages/vue/src/types/utils.ts
+++
@@ -1,6 +0,0 @@
-import type { Component } from "vue";
-
-export type ComponentAs = string | Component;
-
-export type MaybeArray<T> = T | T[];
-export type MaybePromise<T> = T | PromiseLike<T>;
