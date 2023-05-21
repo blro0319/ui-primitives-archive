8f409f02f024325b3fe3a023146ffb6bf58ada75
Blro <gyeongtae020319@gmail.com>
2023-05-21 23:42:50 +0900

    feat(WIP): Validation features

---
+++ packages/vue/src/validate/utils/create-field/index.ts
@@ -0,0 +1,62 @@
+import { computed, type MaybeRefOrGetter, toValue } from "vue";
+import type { Rule } from "~/validate";
+
+export function createField<RuleName extends string, Value>(
+  value: MaybeRefOrGetter<Value>,
+  rules: MaybeRefOrGetter<Rule<RuleName, Value>[]>
+) {
+  const $value = computed(() => toValue(value));
+  const $rules = computed(() => toValue(rules));
+
+  const isRequired = computed(() => {
+    return $rules.value.some((rule) => rule.name === "required");
+  });
+
+  async function validate(): Promise<FieldValidateResult<RuleName>> {
+    const validityState = getDefaultValidityState();
+    const invalidRules: RuleName[] = [];
+
+    const promises = $rules.value.map(async (rule) => {
+      const result = await rule.validator($value.value);
+      validityState[rule.name] = result;
+      if (!result) invalidRules.push(rule.name);
+    });
+    await Promise.all(promises);
+
+    validityState.valid = invalidRules.length === 0;
+
+    return {
+      valid: validityState.valid,
+      state: validityState,
+      invalidRules,
+    };
+  }
+  async function $validate() {
+    return (await validate()).valid;
+  }
+
+  function getDefaultValidityState() {
+    const result: Record<string, boolean> = { valid: true };
+    for (const rule of toValue(rules)) result[rule.name] = false;
+    return result as FieldValidityState<RuleName>;
+  }
+
+  return {
+    value: $value,
+    rules: $rules,
+    isRequired,
+    validate,
+    $validate,
+    getDefaultValidityState,
+  };
+}
+
+export interface FieldValidateResult<RuleName extends string> {
+  valid: boolean;
+  state: FieldValidityState<RuleName>;
+  invalidRules: RuleName[];
+}
+export type FieldValidityState<RuleName extends string> = Record<
+  "valid" | RuleName,
+  boolean
+>;
---
+++ packages/vue/src/validate/utils/index.ts
@@ -0,0 +1,3 @@
+export * from "./create-field";
+export * from "./create-form";
+export * from "./create-rule";
