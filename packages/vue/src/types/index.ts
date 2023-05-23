539f2f691fff0e77246e85665d0274be4c010159
Blro <gyeongtae020319@gmail.com>
2023-05-23 11:59:02 +0900

    feat: camelCase, pascalCase, upperFirst utils

--- packages/vue/src/types/index.ts
+++ packages/vue/src/types/index.ts
@@ -8,3 +8,16 @@ export type AnyFunction = (...args: any[]) => any;
 
 export type MaybeArray<T> = T | T[];
 export type MaybePromise<T> = T | PromiseLike<T>;
+
+export type Prettify<T> = { [K in keyof T]: T[K] } & {};
+
+export type PascalCase<S extends string> = Capitalize<CamelCase<S>>;
+export type CamelCase<S extends string> = Uncapitalize<
+  CamelCaseInner<CamelCaseInner<CamelCaseInner<S, "-">, "_">, " ">
+>;
+type CamelCaseInner<
+  S extends string,
+  D extends string
+> = S extends `${infer L}${D}${infer R}`
+  ? `${L}${Capitalize<CamelCaseInner<R, D>>}`
+  : S;
