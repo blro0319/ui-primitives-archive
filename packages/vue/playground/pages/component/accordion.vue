8296e615efa160e9c64fed0b3e8e6abb152d0a95
Blro <gyeongtae020319@gmail.com>
2023-05-25 11:56:13 +0900

    feat: VAccordionItem open prop

--- packages/vue/playground/pages/component/accordion.vue
+++ packages/vue/playground/pages/component/accordion.vue
@@ -33,7 +33,55 @@ import {
         </VAccordionItem>
       </VAccordion>
     </article>
+    <article>
+      <h2>Opened Item (Single)</h2>
+      <VAccordion>
+        <VAccordionItem v-for="i in [1, 2, 3]" :key="i" :open="i === 2">
+          <VAccordionHeader>
+            <VAccordionTrigger>Item {{ i }}</VAccordionTrigger>
+          </VAccordionHeader>
+          <VAccordionPanel>Panel {{ i }}</VAccordionPanel>
+        </VAccordionItem>
+      </VAccordion>
+    </article>
+    <article>
+      <h2>Opened Item (Multiple)</h2>
+      <VAccordion expand-mode="multiple">
+        <VAccordionItem v-for="i in [1, 2, 3]" :key="i" :open="i !== 1">
+          <VAccordionHeader>
+            <VAccordionTrigger>Item {{ i }}</VAccordionTrigger>
+          </VAccordionHeader>
+          <VAccordionPanel>Panel {{ i }}</VAccordionPanel>
+        </VAccordionItem>
+      </VAccordion>
+    </article>
+    <article>
+      <h2>Transition</h2>
+      <VAccordion>
+        <VAccordionItem v-for="i in [1, 2, 3]" :key="i">
+          <VAccordionHeader>
+            <VAccordionTrigger>Item {{ i }}</VAccordionTrigger>
+          </VAccordionHeader>
+          <Transition name="accordion">
+            <VAccordionPanel>Panel {{ i }}</VAccordionPanel>
+          </Transition>
+        </VAccordionItem>
+      </VAccordion>
+    </article>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.accordion {
+  padding-bottom: 256px;
+
+  &-enter-active,
+  &-leave-active {
+    transition: opacity 250ms;
+  }
+  &-enter-from,
+  &-leave-to {
+    opacity: 0;
+  }
+}
+</style>
