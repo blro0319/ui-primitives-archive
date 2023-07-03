927baaaeada924cb11c6ecfa4b2dfd67a19310e3
Blro <gyeongtae020319@gmail.com>
2023-07-03 19:41:42 +0900

    build: Remove internal functions & types export

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,16 +1,18 @@
-import type { VCompoundType } from "~/types";
-
 export * from "./components";
-export * from "./composables";
-export * from "./constants";
-export * from "./types";
-export * from "./utils";
+export {
+  type UseGlobalCancelStackOptions,
+  type UseGlobalHistoryStackRevokeOptions,
+  type UseVInputOptions,
+  useGlobalCancelStack,
+  useGlobalEscapeStack,
+  useGlobalHistoryStack,
+  useVInput,
+} from "./composables";
+export { COMPONENTS } from "./constants";
+export type { ComponentAs, VBindAttributes } from "./types";
 export * from "./validate";
 
 declare module "vue" {
-  interface ComponentCustomOptions {
-    vCompoundType?: VCompoundType;
-  }
   interface Events {
     // dialog events
     onCancel: Event;
