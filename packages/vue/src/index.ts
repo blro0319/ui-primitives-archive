dbf4f68fc64110ca10dc45483b484e74a674ee6c
Blro <gyeongtae020319@gmail.com>
2023-07-04 19:02:27 +0900

    feat: Exports initial functions & types (Revert)

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,15 +1,7 @@
 export * from "./components";
-export {
-  type UseGlobalCancelStackOptions,
-  type UseGlobalHistoryStackRevokeOptions,
-  type UseVInputOptions,
-  useGlobalCancelStack,
-  useGlobalEscapeStack,
-  useGlobalHistoryStack,
-  useVInput,
-} from "./composables";
-export { COMPONENTS } from "./constants";
-export type { ComponentAs, VBindAttributes } from "./types";
+export * from "./composables";
+export * from "./constants";
+export * from "./types";
 export * from "./validate";
 
 declare module "vue" {
