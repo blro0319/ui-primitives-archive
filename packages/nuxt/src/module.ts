3dea2827a685c2b7806641a90b90caebf954f7dc
Blro <gyeongtae020319@gmail.com>
2023-07-13 17:33:45 +0900

    feat(nuxt): Add composables, rules, types, utils auto imports

--- packages/nuxt/src/module.ts
+++ packages/nuxt/src/module.ts
@@ -1,14 +1,50 @@
-import { addComponent, defineNuxtModule } from "@nuxt/kit";
-import { COMPONENTS } from "@blro/ui-primitives-vue";
+import { addComponent, addImports, defineNuxtModule } from "@nuxt/kit";
+import {
+  COMPONENTS,
+  COMPOSABLES,
+  RULES,
+  TYPES,
+  UTILS,
+} from "@blro/ui-primitives-vue";
 
 export interface ModuleOptions {
-  /**
-   * @default
-   * ```ts
-   * true
-   * ```
-   */
-  components?: boolean;
+  autoImports: {
+    /**
+     * @default
+     * ```ts
+     * true
+     * ```
+     */
+    components?: boolean;
+    /**
+     * @default
+     * ```ts
+     * true
+     * ```
+     */
+    composables?: boolean;
+    /**
+     * @default
+     * ```ts
+     * true
+     * ```
+     */
+    rules?: boolean;
+    /**
+     * @default
+     * ```ts
+     * true
+     * ```
+     */
+    types?: boolean;
+    /**
+     * @default
+     * ```ts
+     * true
+     * ```
+     */
+    utils?: boolean;
+  };
 }
 
 export default defineNuxtModule<ModuleOptions>({
@@ -17,15 +53,64 @@ export default defineNuxtModule<ModuleOptions>({
     configKey: "uiPrimitives",
   },
   defaults: {
-    components: true,
+    autoImports: {
+      components: true,
+      composables: true,
+      rules: true,
+      types: true,
+      utils: true,
+    },
   },
-  setup() {
-    COMPONENTS.forEach((name) => {
-      addComponent({
-        name: name,
-        filePath: "@blro/ui-primitives-vue",
-        export: name,
+  setup({ autoImports }) {
+    if (autoImports.components) {
+      COMPONENTS.forEach((name) => {
+        addComponent({
+          name: name,
+          filePath: "@blro/ui-primitives-vue",
+          export: name,
+        });
       });
-    });
+    }
+
+    if (autoImports.composables) {
+      addImports(
+        COMPOSABLES.map((name) => ({
+          name,
+          as: name,
+          from: "@blro/ui-primitives-vue",
+        }))
+      );
+    }
+
+    if (autoImports.rules) {
+      addImports(
+        RULES.map((name) => ({
+          name,
+          as: name,
+          from: "@blro/ui-primitives-vue",
+        }))
+      );
+    }
+
+    if (autoImports.types) {
+      addImports(
+        TYPES.map((name) => ({
+          name,
+          as: name,
+          from: "@blro/ui-primitives-vue",
+          type: true,
+        }))
+      );
+    }
+
+    if (autoImports.utils) {
+      addImports(
+        UTILS.map((name) => ({
+          name,
+          as: name,
+          from: "@blro/ui-primitives-vue",
+        }))
+      );
+    }
   },
 });
