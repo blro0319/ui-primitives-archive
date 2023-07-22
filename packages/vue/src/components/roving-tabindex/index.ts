341af6111406b197f7ba3684be15937d18f5b5c0
Blro <gyeongtae020319@gmail.com>
2023-07-22 16:59:32 +0900

    fix: MIssing exports (set/useVRovingTabindexContext)

--- packages/vue/src/components/roving-tabindex/index.ts
+++ packages/vue/src/components/roving-tabindex/index.ts
@@ -1,3 +1,4 @@
 export { default as VRovingTabindex } from "./VRovingTabindex.vue";
 export { default as VRovingTabindexItem } from "./VRovingTabindexItem.vue";
-export type * from "./types";
+export * from "./context";
+export * from "./types";
